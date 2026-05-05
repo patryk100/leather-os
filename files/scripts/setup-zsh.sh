c0e797dd29ae6a94e00a8a3e42a7997cde1b7ec7
patryk100 <pperkowski2@gmail.com>
2026-05-05 11:58:10 -0400

    fix setup

--- files/scripts/setup-zsh.sh
+++ files/scripts/setup-zsh.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -e
 
-# Set the default shell for all future users to ZSH
-sed -i 's|SHELL=/bin/bash|SHELL=/usr/bin/zsh|' /etc/default/useradd
+# Change the default shell for all users created in the future
+if [ -f /etc/default/useradd ]; then
+    sed -i 's|^SHELL=.*|SHELL=/usr/bin/zsh|' /etc/default/useradd
+else
+    echo "GROUP=100" > /etc/default/useradd
+    echo "HOME=/var/home" >> /etc/default/useradd
+    echo "SHELL=/usr/bin/zsh" >> /etc/default/useradd
+fi
 
-# Also set it for the root user just in case
-chsh -s /usr/bin/zsh root
+# Also set root's shell using sed since chsh is missing
+sed -i 's|^root:x:0:0:root:/root:/bin/bash|root:x:0:0:root:/root:/usr/bin/zsh|' /etc/passwd
