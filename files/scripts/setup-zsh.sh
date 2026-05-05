20697e1f868a6dfae65327b9b3072f30bfb9c914
patryk100 <pperkowski2@gmail.com>
2026-05-05 11:49:48 -0400

    fix zsh on new users

--- files/scripts/setup-zsh.sh
+++ files/scripts/setup-zsh.sh
@@ -1,1 +1,8 @@
-sudo usermod -s /usr/bin/zsh $USER
+#!/bin/bash
+set -e
+
+# Set the default shell for all future users to ZSH
+sed -i 's|SHELL=/bin/bash|SHELL=/usr/bin/zsh|' /etc/default/useradd
+
+# Also set it for the root user just in case
+chsh -s /usr/bin/zsh root
